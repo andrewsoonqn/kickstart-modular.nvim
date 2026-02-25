c21dc0f2f8c4041f7f132a50650c78201744ff1b
Andrew Soon <andrewsoonqn@gmail.com>
2026-02-25 23:28:53 +0800

    Update conform.lua

--- lua/plugins/lsp/conform.lua
+++ lua/plugins/lsp/conform.lua
@@ -24,7 +24,7 @@ return {
           return nil
         else
           return {
-            timeout_ms = 500,
+            timeout_ms = 1000,
             lsp_format = 'fallback',
           }
         end
@@ -32,7 +32,7 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'isort', 'black' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
